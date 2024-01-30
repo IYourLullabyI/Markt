8c4049a19727849d7b60631527ee481ea04fe934
Gabriel Sikorski <gabriel779@hotmail.de>
2024-01-30 23:17:34 +0100

    sc

--- src/App.js
+++ src/App.js
@@ -15,10 +15,19 @@ const ProductCard = function({product, handleClick}) {
       <div style={{backgroundColor: "lightblue"}}>
         <div style={{margin: "1rem", padding: ".5rem", fontSize: "1.5rem", fontWeight: "700"}}>{product.title}</div>
         <div style={{color: "blue", fontSize: "1.2rem", fontWeight: "500", paddingLeft: "1.5rem", paddingBottom: "1rem"}}>{product.price}€</div>
-        <img src={product.images} alt={product.description} style={{display: "block"}}/>
+        <img src={product.images} alt={product.description} style={{display: "block", width: "100%", textAlign: "center"}}/>
         <button onClick={() => handleClick(product.id, product.title, product.price)} style={{padding: ".5rem", fontWeight: "600", fontSize: "1.2rem", width: "100%", border: "1px solid black", background: "lightYellow", cursor: "pointer"}}>Hinzufügen</button>
       </div>
     </li>
+  )
+}
+
+const Cart = function(cart, count) {
+
+  return (
+    <div>
+        Name: {cart.cart.name} Preis: {cart.cart.price}
+    </div>
   )
 }
 
@@ -26,26 +35,58 @@ function App() {
 const [products, setProducts] = React.useState(initialProduct.products)
 const [count, setCount] = React.useState(0)
 const [cart, setCart] = React.useState([])
-let cartArray = [];
 
 function handleClick(id, title, price) {
   setCount(count + 1);
-  cartArray.push(title, price);
-  setCart(cartArray);
-  console.log(cart);
-
+  setCart((prevCart) => [
+    ...prevCart, {
+      name: title,
+      price: price + " €"
+    }
+  ])
 }
 
-function openCart(){
+function handleCart() {
+  // const [showCart, setCart] = React.useState(false)
 }
+
 
   return (
       <>
-      <h1 className="hl" style={{paddingTop: ".5rem", paddingLeft: "2rem"}}>Obst-Kurier</h1>
-      <div style={{display: "block", fontSize: "1.4rem", fontWeight: "600", textAlign: "center"}}><button onClick={openCart} style={{border: "none", background: "grey", fontSize: "1rem", padding: "5px 20px", cursor: "pointer"}}>Items</button>:{count}</div>
+      <h1 className="hl" style={{
+        paddingTop: ".5rem",
+        paddingLeft: "2rem"
+        }}>
+          Obst-Kurier
+      </h1>
+      <div style={{
+        display: "block",
+        fontSize: "1.4rem",
+        fontWeight: "600",
+        textAlign: "center"
+        }}>
+        <button onClick={handleCart} style={{
+          border: "none",
+          background: "grey",
+          fontSize: "1rem",
+          padding: "5px 20px",
+          cursor: "pointer"
+          }}>Items
+        </button>:{count}
+      </div>
+      <div className='cart' style={{
+        width: "500px",
+        height: "250px",
+        border: "1px solid black",
+        fontSize: ".8rem"
+      }}>{cart.map((x) => (
+        <Cart cart={x} count={count}/>
+      ))}
+      </div> 
       {products.map((x) => (
         <ProductCard product={x} handleClick={handleClick}/>
       ))}
+  	 
       </>
   );
 }
